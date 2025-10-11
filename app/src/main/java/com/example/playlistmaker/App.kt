642a94839f6042caeafbedf0d7d920b4e9d5733f
Vadim Balcer <balcer.v@banki.ru>
2025-10-11 10:33:50 +0300

    16 спринт. Добавил модель для емейла

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -2,7 +2,8 @@ package com.example.playlistmaker
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import com.example.playlistmaker.domain.api.ThemeInteractor
+import com.example.playlistmaker.creator.Creator
+import com.example.playlistmaker.settings.domain.api.ThemeInteractor
 
 class App : Application() {
     lateinit var themeInteractor: ThemeInteractor
