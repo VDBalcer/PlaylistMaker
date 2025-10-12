1fcf92d107ddb3d574d19be2cea6bbb4136e1205
Vadim Balcer <balcer.v@banki.ru>
2025-10-12 13:52:34 +0300

    16 спринт. Поменял хранение и работу с темами

--- app/src/main/java/com/example/playlistmaker/search/data/storage/PrefsStorageClient.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/storage/PrefsStorageClient.kt
@@ -9,26 +9,24 @@ import java.lang.reflect.Type
 
 class PrefsStorageClient<T>(
     private val context: Context,
+    private val dataKey: String,
     private val type: Type
 ) : StorageClient<T> {
 
     private val prefs: SharedPreferences =
-        context.getSharedPreferences(TRACKS_KEY, Context.MODE_PRIVATE)
+        context.getSharedPreferences(dataKey, Context.MODE_PRIVATE)
     private val gson = Gson()
 
     override fun storeData(data: T) {
-        prefs.edit().putString(TRACKS_KEY, gson.toJson(data, type)).apply()
+        prefs.edit().putString(dataKey, gson.toJson(data, type)).apply()
     }
 
     override fun getData(): T? {
-        val dataJson = prefs.getString(TRACKS_KEY, null)
+        val dataJson = prefs.getString(dataKey, null)
         return if (dataJson == null) {
             null
         } else {
             gson.fromJson(dataJson, type)
         }
-    }
-    companion object{
-        const val TRACKS_KEY = "searched_tracks"
     }
 }
