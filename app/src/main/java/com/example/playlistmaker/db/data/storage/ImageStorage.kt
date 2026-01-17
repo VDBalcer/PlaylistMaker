65d7fb4b24f8c565f130584beae7011e5b38684d
Vadim Balcer <balcer.v@banki.ru>
2026-01-17 14:03:19 +0300

    22 спринт. Добавления хранилища картинок

---
+++ app/src/main/java/com/example/playlistmaker/db/data/storage/ImageStorage.kt
@@ -0,0 +1,45 @@
+package com.example.playlistmaker.db.data.storage
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import androidx.core.net.toFile
+import java.io.File
+import java.io.FileOutputStream
+
+class ImageStorage(private val context: Context) {
+
+    fun saveImage(uri: Uri): Uri {
+        if (uri != Uri.EMPTY) {
+            val directory = File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                "playlists"
+            )
+
+            if (!directory.exists()) {
+                directory.mkdirs()
+            }
+
+            val file = File(directory, "cover_${System.currentTimeMillis()}.jpg")
+            //TODO: Подумать над названиями файлов
+
+            context.contentResolver.openInputStream(uri)?.use { input ->
+                FileOutputStream(file).use { output ->
+                    BitmapFactory.decodeStream(input)
+                        .compress(Bitmap.CompressFormat.JPEG, 30, output)
+                }
+            }
+            return Uri.fromFile(file)
+        }
+        return Uri.EMPTY
+    }
+
+    fun deleteImage(path: Uri) {
+        val file = path.toFile()
+        if (file.exists()) {
+            file.delete()
+        }
+    }
+}
